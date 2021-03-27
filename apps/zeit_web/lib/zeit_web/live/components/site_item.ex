c614e7490c862c0bcaeecb252c56b42464a9f8b7
Sultan Iman <sultan.imanhodjaev@gmail.com>
2021-03-27 16:29:48 +0100

    Refactor site card to list

--- apps/zeit_web/lib/zeit_web/live/components/site_item.ex
+++ apps/zeit_web/lib/zeit_web/live/components/site_item.ex
@@ -1,45 +1,40 @@
-defmodule ZeitWeb.Components.SiteCard do
+defmodule ZeitWeb.Components.SiteItem do
   use ZeitWeb, :live_component
 
   @impl true
   def render(assigns) do
     ~L"""
-    <div class="site-card zi-card" id="site-<%= @site.id %>">
-      <h4 class="site-card__title">
-        <%= live_redirect domain(@site.address), to: Routes.site_show_path(@socket, :show, @site) %>
-
+    <div class="site-item zi-note" id="site-<%= @site.id %>">
+      <div>
         <%= if @site.is_archive do %>
-        <span aria-label="Archive mode" data-balloon-pos="up" class="float-right">
+        <span class="site-item__mode" aria-label="Archive mode" data-balloon-pos="up">
           <img class="site-mode" src="<%= Routes.static_path(@socket, "/images/icons/package-white.svg") %>"/>
         </span>
         <% else %>
-        <span aria-label="Monitor mode" data-balloon-pos="up" class="float-right">
+        <span class="site-item__mode" aria-label="Monitor mode" data-balloon-pos="up">
           <img class="site-mode" src="<%= Routes.static_path(@socket, "/images/icons/activity-white.svg") %>"/>
         </span>
         <% end %>
-      </h4>
-
-      <div class="site-card__stat">
-        <p>
-          <span><big><%= @links_count %></big> <small>links</small></span>
-        </p>
-        <p>
-          <span><big><%= @used_space %>, <%= @snapshots_count %></big> <small>snapshots</small></span>
-        </p>
+        <%= live_redirect domain(@site.address), to: Routes.site_show_path(@socket, :show, @site) %>
       </div>
 
-      <div class="site-card__controls">
-        <span aria-label="Add links" data-balloon-pos="down" class="site-card__delete">
+      <div class="site-item__stats">
+        <span><%= @links_count %> links</span> /
+        <span><%= @used_space %> / <%= @snapshots_count %> snapshots</span>
+      </div>
+
+      <div class="site-item__actions">
+        <span aria-label="Add links" data-balloon-pos="down">
           <%= live_patch to: Routes.site_index_path(@socket, :add_links, @site) do %>
             <img src="<%= Routes.static_path(@socket, "/images/icons/plus-white.svg") %>"/>
           <% end %>
         </span>
-        <span class="site-card__edit">
+        <span>
           <%= live_patch to: Routes.site_index_path(@socket, :edit, @site) do %>
             <img src="<%= Routes.static_path(@socket, "/images/icons/edit-white.svg") %>"/>
           <% end %>
         </span>
-        <span class="site-card__delete">
+        <span>
           <%= link to: "#", phx_click: "delete", phx_value_id: @site.id, data: [confirm: "Are you sure?"] do %>
             <img src="<%= Routes.static_path(@socket, "/images/icons/trash-white.svg") %>"/>
           <% end %>
