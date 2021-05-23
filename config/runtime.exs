f00533c98fc6d6adeb85a79c5e18ffd528ab615d
Sultan Iman <sultan.imanhodjaev@gmail.com>
2021-05-23 14:28:33 +0200

    Configure connection pool

--- config/runtime.exs
+++ config/runtime.exs
@@ -10,7 +10,8 @@ database_url =
 config :zeit, Zeit.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "100"),
+  pool: DBConnection.ConnectionPool
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
